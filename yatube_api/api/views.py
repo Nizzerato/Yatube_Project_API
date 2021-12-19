03d8ae3865e847d2dd2374d531dfc545a110aac8
Nizzerato <85722369+Nizzerato@users.noreply.github.com>
2021-12-20 01:36:49 +0300

    Add files via upload

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -44,13 +44,15 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsOwnerOrReadOnly,)
 
-    def get_queryset(self):
+    def get_post(self):
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
-        return post.comments.all()
+        return post
+
+    def get_queryset(self):
+        return self.get_post().comments.all()
 
     def perform_create(self, serializer):
-        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
-        serializer.save(author=self.request.user, post=post)
+        serializer.save(author=self.request.user, post=self.get_post())
 
 
 class FollowViewSet(CreateListViewSet):
